d3c3db829233e2bd54c3cd0e003d52c4576d36ef
sushil sharma <sushil.sharma.y8@gmail.com>
2023-07-07 09:50:26 +0545

    new

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
 
-   &copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!
+   &copy; copyright @ <?= date('Y'); ?> by <span>sushil sharma</span> | all rights reserved!
 
 </footer>
